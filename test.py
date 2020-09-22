71de7b049a636abb1834f5676768cd9be5ad3381
mapena <mapena@gmail.com>
2020-09-22 12:18:14 -0300

    .

--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 from libmate.fmathsum import *
 
 print("from libmate.fmathsum import *")
-print("sumar(2,3):",sumar(2,3)) 
-print("sumar1_0_1(2,3):",sumar1_0_1(2,3))
-
+print("se invoca a sumar(2,3):",sumar(2,3)) 
+print("se invoca a sumar1_0_1(2,3):",sumar1_0_1(2,3))
+print("")
 # pip install paqsumrest
 from libmate import *
 print("from libmate import *")
-print("fmathsum.sumar(2,3)",fmathsum.sumar(2,3)) 
+print("se invoca a fmathsum.sumar(2,3)",fmathsum.sumar(2,3)) 
 
 
 
